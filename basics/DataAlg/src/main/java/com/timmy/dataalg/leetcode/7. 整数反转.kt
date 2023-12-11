c940de59aab878f7609b66c8cd1a7283d1126448
zhuzhonghua <zhuzhonghua@yingzi.com>
2023-12-11 11:02:32 +0800

    整数翻转实现

--- basics/DataAlg/src/main/java/com/timmy/dataalg/leetcode/7. 整数反转.kt
+++ basics/DataAlg/src/main/java/com/timmy/dataalg/leetcode/7. 整数反转.kt
@@ -1,5 +1,7 @@
 package com.timmy.dataalg.leetcode
 
+import kotlin.math.abs
+
 /**
  * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
@@ -22,9 +24,52 @@ package com.timmy.dataalg.leetcode
 输出：0
  */
 fun main() {
+//    val res = reverse(-123)
+    val res = reverse(120)
+    println("res:$res")
 }
 
+/**
+ * 1、审题：
+ * - 输入一个有符号的整数，就是说有可能给的是一个负数，现在需要返回这个整数的翻转结构，并保留符号
+ * -- 题目要求不能存储64位的整数，是不让用long类型数据，不让用那怎么判断Int类型的区间范围呢？
+ * 2、解题
+ * - 先扩展类型，将int类型转换成long类型
+ * - 判断是否是负数，负数的话，转成正整数
+ * - 然后对正整数进行处理，从个位数从后往前处理，每次除于10，并使用结果数字接收，每次乘与10后再加上
+ */
 fun reverse(x: Int): Int {
+    if (x == 0) return 0
+    val isFuShu = x < 0
+    var xL = x.toLong() // Long类型 //
+    xL = abs(xL)
+    var resL: Long = 0 // 存放结果的Long类型变量
 
-    return 0
+    // 不断取出各个数字
+    while (xL != 0L) {
+        val num = xL % 10
+        xL /= 10
+
+        resL = resL * 10 + num
+
+        println("xL:$xL,num:$num,resL:$resL")
+    }
+    resL = if (isFuShu) resL * -1 else resL
+    if (resL > Int.MAX_VALUE || resL < Int.MIN_VALUE) {
+        return 0
+    }
+
+    return resL.toInt()
 }
+
+
+
+
+
+
+
+
+
+
+
+
