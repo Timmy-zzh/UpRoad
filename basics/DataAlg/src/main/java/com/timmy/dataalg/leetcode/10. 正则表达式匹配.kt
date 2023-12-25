a224b0967ef77666718f0a33590bbef2a960b188
timmy <zhuzhonghua@yingzi.com>
2023-12-25 10:17:21 +0800

    字符串匹配

--- basics/DataAlg/src/main/java/com/timmy/dataalg/leetcode/10. 正则表达式匹配.kt
+++ basics/DataAlg/src/main/java/com/timmy/dataalg/leetcode/10. 正则表达式匹配.kt
@@ -1,5 +1,7 @@
 package com.timmy.dataalg.leetcode
 
+import com.timmy.dataalg.print
+
 /**
  * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
 '.' 匹配任意单个字符
@@ -21,24 +23,70 @@ package com.timmy.dataalg.leetcode
 输出：true
 解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
  */
-fun main() {
-//            isMatch("aa", "a")
-//    isMatch("aa", "a*")
-//        isMatch("ab", ".*")
-//        isMatch("aab", "c*a*b")
-        isMatch("mississippi", "mis*is*p*.")
-    println("isMatch:$isMatch")
+fun main() { //            isMatch("aa", "a")
+    //    isMatch("aa", "a*")
+    //        isMatch("ab", ".*")
+    //    val res = isMatch("aab", "c*a*b")
+    val res = isMatch("mississippi", "mis*is*p*.")
+    println("res:$res")
+
+    //    println("isMatch:$isMatch")
 }
 
 /**
  * 解法：动态规划，状态转移方程式
- * -
+ * 1、审题：字符串s，和模式串p，两个进行匹配，主串主要由字符组成，模式串由字符和通配符*和.共同组成
+ * - 其中通配符.匹配任意一个字符，通配符*匹配零个或多个前面的字符
+ * - 所以通配符*，可以与前面的一个字符组合作为一个空字符，也可以作为多个相同字符xx进行匹配
+ * 2、解题：
+ * - 状态表采用二维数组dp，使用boolean类型，二维数组的维度分别表示主串和字符串的长度，
+ * - dp[i][j] 表示主串s第i个字符与模式串p第j个字符，是否进行进行匹配
+ * -- 他们之间是存在递进关系的
+ *
+ * - 情况1：主串和模式串串都是字符，这种情况需要两个字符都相等，dp值才为true
+ * - 情况2：模式串字符是通配.，则模式串可与主串任意字符匹配
+ * - 情况3：模式串是通配符* ，则需要匹配前面字符0个或多个情况，dp[i] = dp[i-2]
+ * -- 多个情况，需要p[j-2] 与s[j] 字符相等
+ * 3、dp值以长度为标准，所以dp[0][0]=true
+ * - dp的元素值，标识主串s不同长度字符，与模式串p不同长度字符下，的匹配情况
  */
 fun isMatch(s: String, p: String): Boolean {
-    println("s=$s,p=$p")
-    isMatch = false
-    strMatch(0, 0, s, p)
-    return isMatch
+    val sLen = s.length
+    val pLen = p.length
+    println("s=$s ,sLen=$sLen,p=$p, pLen=$pLen")
+
+    val dp = Array(sLen + 1) {
+        BooleanArray(pLen + 1) { false }
+    }
+    dp[0][0] = true // 主串与模式串长度都为0时，匹配结果为true
+    // 第一行处理
+    for (j in 1..pLen) {
+        if (p[j - 1] == '*') {
+            dp[0][j] = dp[0][j - 2]
+        }
+    }
+//    dp.print()
+
+    for (i in 1..sLen) {    // 如果主串是空的，模式串p是一个点号".",他们也是匹配的，所以主串从0开始
+        for (j in 1..pLen) {
+            if (p[j - 1] == '.') {   // 情况1：模式串可以匹配任意一个字符
+                dp[i][j] = dp[i - 1][j - 1]
+            } else if (p[j - 1] == '*') {   // 情况2：模式串字符是星号*，可匹配零个或多个前一个字符
+                // 先匹配0个字符
+                dp[i][j] = dp[i][j - 2] // 匹配多个，需要主串的遍历字符与模式串*前面的一个字符相同
+
+                if ((p[j - 2] == s[i - 1] || p[j - 2] == '.')) { // 主串的字符和模式串前一个字符能够匹配的情况
+                    dp[i][j] = dp[i][j] || dp[i - 1][j]
+                }
+            } else if (p[j - 1] == s[i - 1]) { // 情况3：都是字符，要相等才行,第一行只匹配特殊字符
+                dp[i][j] = dp[i - 1][j - 1]
+            }
+        }
+//        println("i=$i")
+//        dp.print()
+    }
+
+    return dp[sLen][pLen]
 }
 
 /**
