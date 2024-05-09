81d4af4484f3dc32c01ff37703dfa7a286be05e2
timmy <zhuzhonghua@yingzi.com>
2024-05-09 10:18:11 +0800

    二叉树的公共结点，树结构

--- basics/DataAlg/src/main/java/com/timmy/dataalg/leetcode/236. 二叉树的最近公共祖先.kt
+++ basics/DataAlg/src/main/java/com/timmy/dataalg/leetcode/236. 二叉树的最近公共祖先.kt
@@ -33,40 +33,89 @@ fun main() {
 }
 
 /**
+ * 2、dfs解法
+ * - 不断往下遍历，判断当前遍历的结点值，是否有和p/q其中一个相等的节点值
+ * - 在遍历的过程中，查找最近公共祖先节点，
+ * - 分两种情况：
+ * -- 根节点匹配p/q其中一个节点，左右子树中有一个匹配
+ * -- 根节点没有匹配其中任何一个，左右子树分别匹配
+ * 3、总结
+ * - dfs深度优先遍历，判断当前遍历的结点，是否有和p，q相匹配的结点值
+ * - 先往下不断遍历，拿到左右子树的返回值，通过返回值直到左右子树是否有匹配结果
+ * - 递归的方法，也需要返回当前结果，标记当前结点是否有匹配的情况
+ */
+var ans: TreeNode? = null
+fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+    dfs236(root, p, q)
+    return ans
+}
+
+fun dfs236(root: TreeNode?, p: TreeNode?, q: TreeNode?): Boolean {
+    if (root == null) {
+        return false
+    } // 左右子树是否匹配
+    val leftC = dfs236(root.left, p, q)
+    val rightC = dfs236(root.right, p, q)
+
+    if ((leftC && rightC) || ((leftC || rightC) && (root.`val` == p!!.`val` || root.`val` == q!!.`val`))) {
+        ans = root
+    }
+
+    return leftC || rightC || (root.`val` == p!!.`val` || root.`val` == q!!.`val`)
+}
+
+/**
  * 1、审题：
  * - 输入一个二叉树和其中的两个节点，求这两个节点的最近公共祖先，
  * 2、解题
  * - 二分查找法，将二叉树分为两个部分，（左子树+根节点）和（右子树）
  * - 判断两部分中是否有其中一个节点值，有的话，则返回当前根节点为所求值
- * -
+ * 3、思路总结：
+ * - 考虑三种情况，根节点匹配其中一个元素，则左右子树其中一个匹配即可
+ * - 根节点不匹配，左右子树必须全部匹配中
+ * 4、超过时间限制
+ * - 注意裁剪操作
  */
-fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+fun lowestCommonAncestor2(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+    println("lowestCommonAncestor2 root:${root?.`val`},p:${p?.`val`},q:${q?.`val`}")
     if (root == null) {
         return null
     }
-    val leftC = root.`val` == p!!.`val` || isContainOne(root.left, p, q!!)
-    val rightC = isContainOne(root.right, p, q!!)
-    if (leftC && rightC) {
-        return root
+    val rootC = (root.`val` == p!!.`val` || root.`val` == q!!.`val`)
+    val leftC = if (root.left != null) isContainOne(root.left, p, q!!) else false
+    val rightC = if (root.right != null) isContainOne(root.right, p, q!!) else false
+
+    // 有一种情况，根节点匹配一个数字，左子树中一个节点匹配一个数字，那就只能是leftC为true，rightC为false
+    println("lowestCommonAncestor2 rootC:$rootC, leftC:$leftC,rightC:$rightC")
+    if (rootC) {
+        if (leftC || rightC) {
+            return root
+        }
+    } else {
+        if (leftC && rightC) {
+            return root
+        }
     }
 
-    val leftNode = lowestCommonAncestor(root.left, p, q)
-    val rightNode = lowestCommonAncestor(root.right, p, q)
-    if (leftNode != null) {
-        return leftNode
+    if (leftC) {
+        return lowestCommonAncestor2(root.left, p, q)
     }
-    if (rightNode != null) {
-        return rightNode
-    }
-    return null
+    return lowestCommonAncestor2(root.right, p, q)
 }
 
 fun isContainOne(root: TreeNode?, p: TreeNode, q: TreeNode): Boolean {
+    println("isContainOne root:${root?.`val`},p:${p?.`val`},q:${q?.`val`}")
     if (root == null) {
         return false
     }
     if (root.`val` == p.`val` || root.`val` == q.`val`) {
         return true
     }
-    return isContainOne(root.left, p, q) || isContainOne(root.right, p, q)
+    return if (root.left != null && root.right != null) {
+        isContainOne(root.left, p, q) || isContainOne(root.right, p, q)
+    } else if (root.left != null) {
+        isContainOne(root.left, p, q)
+    } else {
+        isContainOne(root.right, p, q)
+    }
 }
